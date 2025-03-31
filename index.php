eaed6ebdeb27c06971b61673855506a53a3a1568
jerryvien <jerryyee0902@gmail.com>
2025-03-31 22:46:25 +0800

    validate

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <script src="https://www.google.com/recaptcha/api.js" async defer></script>
 </head>
 <body>
-  <form action="" method="post">
+  <form action="validate_captcha.php" method="post">
     <div class="g-recaptcha" data-sitekey="6LeVaQUrAAAAALa-hI5v-ZyAoZLUDMJeYBvBw_bZ"></div>
     <br/>
     <input type="submit" value="Submit">
