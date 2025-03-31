4fd375e04527ba5877af4419f477477cdcfe8be9
jerryvien <jerryyee0902@gmail.com>
2025-03-31 22:03:54 +0800

    update to captha 3

--- index.php
+++ index.php
@@ -1,19 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Google Form with reCAPTCHA</title>
-    <!-- Load reCAPTCHA API -->
-    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
-</head>
-<body>
-    <h1>Submit Your Inquiry</h1>
-    <form action="validate_captcha.php" method="post">
-        <!-- Google reCAPTCHA Widget -->
-        <div class="g-recaptcha" data-sitekey="6LdLZAUrAAAAAPZCS2ZdeOQeGuQDYVHD1qmuxGD_"></div>
-        <br>
-        <input type="submit" value="Submit">
-    </form>
-</body>
-</html>
+<!-- Google reCAPTCHA v3 -->
+<script src="https://www.google.com/recaptcha/api.js?render=YOUR_SITE_KEY"></script>
 
+<form action="validate_captcha.php" method="post" id="recaptchaForm">
+    <input type="hidden" name="g-recaptcha-response" id="g-recaptcha-response">
+    <input type="submit" value="Submit">
+</form>
+
+<script>
+    grecaptcha.ready(function() {
+        grecaptcha.execute('6LdLZAUrAAAAAPZCS2ZdeOQeGuQDYVHD1qmuxGD_', {action: 'submit'}).then(function(token) {
+            document.getElementById('g-recaptcha-response').value = token;
+        });
+    });
+</script>
